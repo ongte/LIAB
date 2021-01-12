6a9925a462e9735a5d12f3de6adf162fd9f9a6c4
[Aaron.Southerland] <[kd5mvd@gmail.com]>
2021-01-12 15:53:05 -0600

    added some formatting to post install script

--- prep.sh
+++ prep.sh
@@ -4,8 +4,13 @@ PITD=`mktemp -d`
 LOG="${PITD}/postinstall.log"
 #lets create a copy of the iso to use in the environemt
 echo "Please ensure that your CentOS Installation ISO is attached to your VM!"
+echo " " 
 echo "Creating copy of your ISO, please be patient as this will take a few mintues!"
 cp /dev/sr0 /root/centos.iso
+echo " " 
+echo "Installation Media ISO created successfully!"
+echo " " 
+echo "Setting up PostInstall Script Environment"
 #this command will install git on the server
 yum install -y git &>>"${LOG}"
 #this command will give a temporary working directory
@@ -13,6 +18,8 @@ OUT="$(mktemp -d /run/tmp.XXX)"
 cd $OUT
 #this command will clone the LIAB repo to the local machine 
 git clone https://github.com/aksoutherland/LIAB &>>"${LOG}"
+echo " " 
+echo "Beginning Post Install Script"
 #this command will kickoff the postinstall script
 $OUT/LIAB/postinstall.sh
 
