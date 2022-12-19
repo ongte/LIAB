d32c341c0030a7073981b0c68f3b6163fca77dfc
root <root@server1.example.com>
2022-12-19 03:35:59 -0600

    Fixed registry script 1

--- registry1.sh
+++ registry1.sh
@@ -9,5 +9,6 @@ su - student << 'EOF'
 export XDG_RUNTIME_DIR=/run/user/$(id -u student)
 export DBUS_SESSION_BUS_ADDRESS=/run/user/$(id -u student)/bus
 mkdir -pv ~/registry &>> /dev/null
+podman run -d --name registry -p 5000:5000 -v ~/registry:/var/lib/registry:Z registry:2 &>> /dev/null
 EOF
 echo "User podman configured."
