d51402f64f213f53d8a575af0e4518f306770900
root <root@server1.example.com>
2022-12-19 03:48:37 -0600

    Added delay on registry script

--- registry2.sh
+++ registry2.sh
@@ -9,6 +9,7 @@ export DBUS_SESSION_BUS_ADDRESS=/run/user/$(id -u student)/bus
 mkdir -pv ~/registry &>> /dev/null
 podman run -d --name registry -p 5000:5000 -v ~/registry:/var/lib/registry:Z registry:2 &>> /dev/null
 EOF
+sleep 5
 #here we create our local registry and activate it
 su - student << 'EOF'
 export XDG_RUNTIME_DIR=/run/user/$(id -u student)
